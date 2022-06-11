358e25d08cb752a240714e5eb43dbff4e631282f
jumha <e24076750@gs.ncku.edu.tw>
2022-06-11 16:04:22 +0800

    Fix rotate bits from 3 to 10 for w[i-2]

--- hdl/preprocess.v
+++ hdl/preprocess.v
@@ -14,8 +14,8 @@ module preprocess(
     reg [`W_WIDTH-1:0] x_w15, x_w2, w16, w7;
     reg [`W_WIDTH-1:0] x_w14, x_w1, w15, w6;
 
-    wire [`W_WIDTH-1:0] w15rr7, w15rr18, w15rr3, w2rr17, w2rr19, w2rr3;
-    wire [`W_WIDTH-1:0] w14rr7, w14rr18, w14rr3, w1rr17, w1rr19, w1rr3;
+    wire [`W_WIDTH-1:0] w15rr7, w15rr18, w15rr3, w2rr17, w2rr19, w2rr10;
+    wire [`W_WIDTH-1:0] w14rr7, w14rr18, w14rr3, w1rr17, w1rr19, w1rr10;
 
     assign w15rr7  = {data_reg[7][38:32], data_reg[7][63:39]};
     assign w15rr18 = {data_reg[7][49:32], data_reg[7][63:50]};
@@ -23,7 +23,7 @@ module preprocess(
 
     assign w2rr17 = {data_reg[0][16:0], data_reg[0][31:17]};
     assign w2rr19 = {data_reg[0][18:0], data_reg[0][31:19]};
-    assign w2rr3  = {data_reg[0][2:0], data_reg[0][31:3]};
+    assign w2rr10 = {data_reg[0][9:0], data_reg[0][31:10]};
 
     assign w14rr7  = {data_reg[6][6:0], data_reg[6][31:7]};
     assign w14rr18 = {data_reg[6][17:0], data_reg[6][31:18]};
@@ -31,7 +31,7 @@ module preprocess(
 
     assign w1rr17 = {data_reg[0][48:32], data_reg[0][63:49]};
     assign w1rr19 = {data_reg[0][50:32], data_reg[0][63:51]};
-    assign w1rr3  = {data_reg[0][34:32], data_reg[0][63:35]};
+    assign w1rr10 = {data_reg[0][41:32], data_reg[0][63:42]};
 
     assign w_o = {x_w14 ^ x_w1 ^ w15 ^ w6, x_w15, x_w2, w16, w7};
 
@@ -63,11 +63,11 @@ module preprocess(
             data_reg[1] <= data_reg[0];
             data_reg[0] <= data_i;
             x_w15 <= w15rr7 ^ w15rr18 ^ w15rr3;
-            x_w2  <= w2rr17 ^ w2rr19 ^ w2rr3;
+            x_w2  <= w2rr17 ^ w2rr19 ^ w2rr10;
             w16   <= data_reg[7][31:0];
             w7    <= data_reg[3][63:32];
             x_w14 <= w14rr7 ^ w14rr18 ^ w14rr3;
-            x_w1  <= w1rr17 ^ w1rr19 ^ w1rr3;
+            x_w1  <= w1rr17 ^ w1rr19 ^ w1rr10;
             w15   <= data_reg[7][63:32];
             w6    <= data_reg[2][31:0];
         end
